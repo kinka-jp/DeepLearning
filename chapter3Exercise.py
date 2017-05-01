75e263f53695c2b1c3402900bdad583c55f8a142
tan kinka <lunwu.xiaoxia@gmail.com>
2017-05-01 12:40:16 +0900

    ステップ関数　＆　シグモイド関数

---
+++ chapter3Exercise.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+import numpy as np
+import matplotlib.pylab as plt
+
+def step_function(x):
+    return np.array(x>0,dtype=np.int)
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+x = np.arange(-5.0,5.0,0.1)
+y= sigmoid(x) #シグモイド関数
+#y = step_function(x)　#ステップ関数
+plt.plot(x,y)
+plt.ylim(-0.1,1.1) #range of y axis
+plt.show()
